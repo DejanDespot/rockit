f5e45cb0e061a2153c0f70b04d78dfd6d7c5a726
DejanDespot <dejandespot7@gmail>
2019-08-28 21:28:40 +0200

    Sidebar

--- src/Views/MainView.js
+++ src/Views/MainView.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import styles from '../Styles/main_view.scss'
 import NavBar from "../Components/NavBar";
+import SideBar from "../Components/SideBar";
 
 class MainView extends Component {
     render() {
         return (
             <div className={styles.mainView}>
-                <NavBar/>
+                <NavBar />
+                <SideBar />
             </div>
         );
     }
