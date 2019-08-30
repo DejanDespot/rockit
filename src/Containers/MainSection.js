5fbce52cb5218b9542408c7341447af4412eeb51
DejanDespot <dejandespot7@gmail>
2019-08-30 08:44:00 +0200

    Main Section, first half

---
+++ src/Containers/MainSection.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styles from '../Styles/main_section.scss';
+import PlaylistImage from '../Assets/Images/coby.jpg';
+import Options from '../Assets/Icons/options.svg';
+
+class MainSection extends Component {
+    render() {
+        return (
+            <div className={styles.mainSection}>
+                <div className={styles.mainBlock}>
+                    <img src={PlaylistImage} className={styles.mainImage}/>
+                    <div className={styles.infoAndButtons}>
+                        <div className={styles.info}>
+                            <p>Playlist</p>
+                            <div className={styles.playlistName}>Top pop 2019</div>
+                            <div className={styles.playlistNumbers}>
+                                <div>10 songs</div>
+                                <div>45 minutes</div>
+                            </div>
+                        </div>
+                        <div className={styles.buttons}>
+                            <div className={styles.play}>Play</div>
+                            <img src={Options} />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.mainBlock}>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MainSection;
--- src/Views/MainView.js
+++ src/Views/MainView.js
@@ -3,14 +3,18 @@ import styles from '../Styles/main_view.scss'
 import NavBar from "../Components/NavBar";
 import SideBar from "../Containers/SideBar";
 import ControlBar from '../Containers/ControlBar';
+import MainSection from '../Containers/MainSection';
 
 class MainView extends Component {
     render() {
         return (
             <div className={styles.mainView}>
-                <NavBar />
-                <SideBar />
-                <ControlBar />
+                <NavBar className={styles.NavBar} />
+                <div className={styles.middle}>
+                    <SideBar className={styles.SideBar} />
+                    <MainSection className={styles.MainSection} />
+                </div>
+                <ControlBar className={styles.ControlBar} />
             </div>
         );
     }
