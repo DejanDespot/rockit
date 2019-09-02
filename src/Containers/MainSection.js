df3c503913407e2c0a6b817e0cd74e52a31af163
DejanDespot <dejandespot7@gmail>
2019-09-02 08:43:23 +0200

    Main Section

--- src/Containers/MainSection.js
+++ src/Containers/MainSection.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styles from '../Styles/main_section.scss';
 import PlaylistImage from '../Assets/Images/coby.jpg';
 import Options from '../Assets/Icons/options.svg';
+import FilterImg from '../Assets/Icons/magnifying-glass.svg';
 
 class MainSection extends Component {
     render() {
@@ -24,8 +25,70 @@ class MainSection extends Component {
                         </div>
                     </div>
                 </div>
+                {/* <div className={classNames(styles.mainBlock, styles.lastBlock)}> */}
                 <div className={styles.mainBlock}>
-
+                    <div className={styles.filter}>
+                        <img src={FilterImg} /> 
+                        <p>Filter</p>
+                    </div>
+                    <div className={styles.list}>
+                        <div className={styles.infoLabels}>
+                            <div>#</div>
+                            <div>Title</div>
+                            <div>Artist</div>
+                            <div>Duration</div>
+                        </div>
+                        <div className={styles.songs}>
+                            <div className={styles.song}>
+                                <div>1</div>
+                                <div>Leto je</div>
+                                <div>Coby</div>
+                                <div>3:23</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>2</div>
+                                <div>Biseri od zlata</div>
+                                <div>Coby</div>
+                                <div>3:16</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>3</div>
+                                <div>Samo jako</div>
+                                <div>Coby feat. Stoja</div>
+                                <div>4:16</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>4</div>
+                                <div>Zver</div>
+                                <div>Coby</div>
+                                <div>3:06</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>5</div>
+                                <div>Lukas</div>
+                                <div>Coby</div>
+                                <div>3:29</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>6</div>
+                                <div>Title</div>
+                                <div>Coby feat. Senidah</div>
+                                <div>2:59</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>7</div>
+                                <div>Mami</div>
+                                <div>Coby</div>
+                                <div>3:06</div>
+                            </div>
+                            <div className={styles.song}>
+                                <div>8</div>
+                                <div>Rambo</div>
+                                <div>Coby</div>
+                                <div>3:38</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
