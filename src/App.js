312e889029478ad1131db0fcefeadb9d1a62fdc5
DejanDespot <dejandespot7@gmail.com>
2019-09-24 21:44:13 +0200

    Responsive mixin and event listener

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import { hot } from 'react-hot-loader/root';
 import MainView from "./Views/MainView";
 
 class App extends Component {
+
+    state = {
+        isMobile : window.innerWidth < 768
+    };
+
+    componentDidMount() {
+        console.log(this.state.isMobile);
+        window.addEventListener('resize', this.handleResize)
+    }
+
+    handleResize = () => {
+        let width = window.innerWidth;
+        if (width < 768 && !this.state.isMobile) {
+            this.setState({
+                isMobile: true
+            })
+        }
+
+        else if (width > 768 && this.state.isMobile) {
+            this.setState({
+                isMobile: false
+            })
+        }
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize)
+    }
+
     render() {
         return (
             <div style={{width: '100%', height: '100%'}}>
