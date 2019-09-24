e436c8f7d6dc826b013ff8a6a3e0c20d13f1effd
DejanDespot <dejandespot7@gmail.com>
2019-09-24 22:14:02 +0200

    Redux installation and configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
 import styles from './styles/main.scss';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+});
+
+const store = createStore(rootReducer(
+    applyMiddleware(thunk)
+));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
 
 const wrapper = document.getElementById("myApp");
-wrapper ? ReactDOM.render(<App/>, wrapper) : false;
+wrapper ? ReactDOM.render(app, wrapper) : false;
