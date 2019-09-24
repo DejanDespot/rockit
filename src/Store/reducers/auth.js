74534459505fcd2be6a49d96c28cab3a98975de9
DejanDespot <dejandespot7@gmail.com>
2019-09-24 22:53:42 +0200

    Default auth reducer init

---
+++ src/Store/reducers/auth.js
@@ -0,0 +1,15 @@
+import {updateObject} from "../../Utils";
+
+const initialState = {
+    loggedIn: false,
+    loading: false
+};
+
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
